7f63ccf57f740f78e7153ccbcc79773d8865e22b
bastienapp <29610890+bastienapp@users.noreply.github.com>
2023-06-01 13:16:41 +0200

    clean

--- src/app/input-component/input-component.component.ts
+++ src/app/input-component/input-component.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-input-component',
